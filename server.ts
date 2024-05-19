a434981b0f880be1bd4ff1156a8b0ddc53fcccb0
AndroVision <91982670+DarkSword7@users.noreply.github.com>
2024-05-19 19:00:10 +0530

    chore: Add @types/express dependency and update npm dependencies and created src folder with app.ts file

--- server.ts
+++ server.ts
@@ -1,1 +1,11 @@
-console.log("welcome to server.ts");
+import app from "./src/app";
+
+const startServer = () => {
+  const port = process.env.PORT || 3000;
+
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+};
+
+startServer();
