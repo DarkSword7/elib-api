b9775f3822bfdecc5743e7f13bdc40d7ffa1f91f
AndroVision <91982670+DarkSword7@users.noreply.github.com>
2024-05-19 19:19:22 +0530

    chore: Update npm dependencies and add dotenv configuration

--- server.ts
+++ server.ts
@@ -1,10 +1,8 @@
 import app from "./src/app";
-import dotenv from "dotenv";
-
-dotenv.config();
+import { config } from "./src/config/config";
 
 const startServer = () => {
-  const port = process.env.PORT || 3000;
+  const port = config.port || 3000;
 
   app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
