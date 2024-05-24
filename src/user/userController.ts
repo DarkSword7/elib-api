070f5260b9a6943e99d14cfc711901042896e80c
AndroVision <91982670+DarkSword7@users.noreply.github.com>
2024-05-24 17:14:51 +0530

    feat: Add user model and validation in userController

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import createHttpError from "http-errors";
+import userModel from "./userModel";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   const { name, email, password } = req.body;
@@ -9,6 +10,15 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     const error = createHttpError(400, "All fields are required!");
     return next(error);
   }
+
+  // Check if user already exists
+  const user = await userModel.findOne({ email });
+
+  if (user) {
+    const error = createHttpError(400, "User already exists with this email!");
+    return next(error);
+  }
+
   // Process the request
   // Send the response
   res.json({
---
+++ src/user/userModel.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import { User } from "./userTypes";
+
+const userSchema = new mongoose.Schema<User>(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required!"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required!"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required!"],
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
