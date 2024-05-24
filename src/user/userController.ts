7381387ecd931fa74471e5acc15a21d3375ea5a6
AndroVision <91982670+DarkSword7@users.noreply.github.com>
2024-05-24 18:25:41 +0530

    feat: Implement user login functionality with JWT token generation

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -61,9 +61,50 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
 };
 
 const loginUser = async (req: Request, res: Response, next: NextFunction) => {
-  res.json({
-    message: "Login successful!",
-  });
+  const { email, password } = req.body;
+
+  // Validation
+  if (!email || !password) {
+    const error = createHttpError(400, "All fields are required!");
+    return next(error);
+  }
+
+  // Check if user exists
+  const user = await userModel.findOne({ email });
+  try {
+    if (!user) {
+      const error = createHttpError(404, "User not found!");
+      return next(error);
+    }
+  } catch (error) {
+    return next(createHttpError(500, "Error while getting user"));
+  }
+
+  // Process the request
+
+  try {
+    // Check password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      const error = createHttpError(401, "Invalid credentials!");
+      return next(error);
+    }
+  } catch (error) {
+    return next(createHttpError(500, "Error while comparing passwords"));
+  }
+
+  // Token generation JWT
+  try {
+    const token = sign({ sub: user._id }, config.jwtSecret as string, {
+      expiresIn: "7d",
+      algorithm: "HS256",
+    });
+    res.json({
+      accessToken: token,
+    });
+  } catch (error) {
+    return next(createHttpError(500, "Error while creating JWT token"));
+  }
 };
 
 export { createUser, loginUser };
