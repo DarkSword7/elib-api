3b8721305a798128252a54f994b87f6fcc59f4d6
AndroVision <91982670+DarkSword7@users.noreply.github.com>
2024-05-24 17:23:00 +0530

    chore: Update npm dependencies and add bcrypt for password hashing

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import createHttpError from "http-errors";
 import userModel from "./userModel";
+import bcrypt from "bcrypt";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   const { name, email, password } = req.body;
@@ -18,6 +19,9 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     const error = createHttpError(400, "User already exists with this email!");
     return next(error);
   }
+
+  // Create new user
+  const hashedPassword = await bcrypt.hash(password, 12);
 
   // Process the request
   // Send the response
