1ec302585099e7eb8f9caf0fc7f7cb6c980561ad
AndroVision <91982670+DarkSword7@users.noreply.github.com>
2024-05-24 18:57:40 +0530

    chore: Add multer dependency and implement file upload functionality in bookRouter

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
+  const {} = req.body;
   res.json({
     message: "Create book route",
   });
--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -1,9 +1,29 @@
+import path from "node:path";
 import { Router } from "express";
 import { createBook } from "./bookController";
+import multer from "multer";
 
 const bookRouter = Router();
 
+const upload = multer({
+  dest: path.resolve(__dirname, "../../public/data/uploads"),
+  limits: { fileSize: 3e7 },
+});
+
 // Routes
-bookRouter.post("/", createBook);
+bookRouter.post(
+  "/",
+  upload.fields([
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+    {
+      name: "file",
+      maxCount: 1,
+    },
+  ]),
+  createBook
+);
 
 export default bookRouter;
