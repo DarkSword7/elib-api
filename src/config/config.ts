03b97e4a3e283229cc853a548d1bd04872e58980
AndroVision <91982670+DarkSword7@users.noreply.github.com>
2024-05-24 18:01:19 +0530

    Sign JWT Token

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,6 +6,7 @@ const _config = {
   port: process.env.PORT,
   mongoURI: process.env.MONGO_URI,
   env: process.env.NODE_ENV,
+  jwtSecret: process.env.JWT_SECRET,
 };
 
 export const config = Object.freeze(_config);
--- src/user/userController.ts
+++ src/user/userController.ts
@@ -2,6 +2,9 @@ import { Request, Response, NextFunction } from "express";
 import createHttpError from "http-errors";
 import userModel from "./userModel";
 import bcrypt from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { config } from "../config/config";
+import { access } from "fs";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   const { name, email, password } = req.body;
@@ -20,13 +23,26 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     return next(error);
   }
 
+  // Process the request
+
   // Create new user
   const hashedPassword = await bcrypt.hash(password, 12);
 
-  // Process the request
+  const newUser = await userModel.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  // Token generation JWT
+  const token = sign({ sub: newUser._id }, config.jwtSecret as string, {
+    expiresIn: "7d",
+    algorithm: "HS256",
+  });
+
   // Send the response
   res.json({
-    message: "User registered successfully!",
+    accessToken: token,
   });
 };
 
