df86a9560c119685ca1bfc48d14b59f15f3b88c8
AndroVision <91982670+DarkSword7@users.noreply.github.com>
2024-05-25 16:37:39 +0530

    feat: Add updateBook functionality to bookController and bookRouter

--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -1,6 +1,6 @@
 import path from "node:path";
 import { Router } from "express";
-import { createBook } from "./bookController";
+import { createBook, updateBook } from "./bookController";
 import multer from "multer";
 import authenticate from "../middlewares/authenticate";
 
@@ -26,6 +26,22 @@ bookRouter.post(
     },
   ]),
   createBook
+);
+
+bookRouter.patch(
+  "/:bookId",
+  authenticate,
+  upload.fields([
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+    {
+      name: "file",
+      maxCount: 1,
+    },
+  ]),
+  updateBook
 );
 
 export default bookRouter;
